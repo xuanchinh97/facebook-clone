0a9875c4e9ab47af62199cd91fe5fee7d7bed643
chinhdz97 <kqtchcheng97@gmail.com>
2022-05-01 23:32:59 +0700

    login form

---
+++ assets/main.js
@@ -0,0 +1,25 @@
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+// gender select 
+if (window.laction.pathname === '/')  {
+    const radioBtn1 = document.querySelector("#flexRadioDefault1")
+    const radioBtn2 = document.querySelector("#flexRadioDefault2")
+    const radioBtn3 = document.querySelector("#flexRadioDefault3")
+    const genderSelect = document.querySelector("#genderSelect")
+
+    radioBtn1.addEventListener("change", ()=> {
+        genderSelect.classList.add("d-none")
+    })
+    
+    radioBtn2.addEventListener("change", ()=> {
+        genderSelect.classList.add("d-none")
+    })
+    
+    radioBtn3.addEventListener("change", ()=> {
+        genderSelect.classList.remove("d-none")
+    })
+    
+}
